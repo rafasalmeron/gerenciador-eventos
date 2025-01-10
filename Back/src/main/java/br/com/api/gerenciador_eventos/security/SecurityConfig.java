fb7045ccdaa1dfca569f61a8f04880141d275d58
Rafael Salmeron <rafasalmeronjr@gmail.com>
2025-01-10 03:03:07 -0300

    Update SecurityConfig.java

--- Back/src/main/java/br/com/api/gerenciador_eventos/security/SecurityConfig.java
+++ Back/src/main/java/br/com/api/gerenciador_eventos/security/SecurityConfig.java
@@ -44,7 +44,7 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://gerenciador-eventos-gamma.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
