0f04aed14d564b0143c89e0074545775e595840e
Rafael Salmeron <rafasalmeronjr@gmail.com>
2025-01-10 03:00:27 -0300

    Update api.jsx

--- React/src/service/api.jsx
+++ React/src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:9000'
+    baseURL: 'https://gerenciador-eventos-7q10.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
