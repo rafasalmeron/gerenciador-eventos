b86281ea7697da192dbf1066b78956c11cecf89c
Rafael Salmeron <rafasalmeronjr@gmail.com>
2025-01-12 23:01:15 -0300

    Update AuthContext.jsx

--- React/src/context/AuthContext.jsx
+++ React/src/context/AuthContext.jsx
@@ -45,7 +45,7 @@ export const AuthProvider = ({ children }) => {
     };
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, userInfo, login, logout }}>
+        <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
             {children}
         </AuthContext.Provider>
     );
